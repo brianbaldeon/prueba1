657659affc138b2c02be6274c744cf2fe290d24f
Kanon123 <108494647+brianbaldeon@users.noreply.github.com>
2023-08-22 23:03:51 -0300

    contacto
    Contacto, sin hooks

---
+++ Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css';
+
+export function Footer(){
+    return(
+        
+            
+            <footer><span>FCAD  UNER</span></footer>
+        
+        
+        
+    );
+}
